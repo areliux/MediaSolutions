c1f221c4068abdc37a9202950da8692062fe5f57
areliux <zulare_1424@hotmail.com>
2014-02-21 10:41:20 -0600

    demo para android

---
+++ www/Scripts/Modules/Models/keywords.js
@@ -0,0 +1,30 @@
+﻿$(document).on('pageshow', '#keywords', function () {
+    console.log("Inicializando palabras clave...");
+    var tagWidth;
+    var tagHeight;
+    var tagMaxSize;
+    tagWidth = $("#tagcloud").width();
+    tagHeight = $("#tagcloud").height();
+    tagMaxSize = (tagWidth >= tagHeight) ? tagWidth : tagHeight;
+    console.log(tagWidth);
+    console.log(tagHeight);
+    console.log(tagMaxSize);
+    $('#tagcloud-canvas').width(tagMaxSize);
+    //$('#tagcloud-canvas').height(tagHeight);
+    if (!$('#tagcloud-canvas').tagcanvas({
+        textColour: 'rgb(45,74, 128)',
+        outlineThickness: 1,
+        maxSpeed: 0.03,
+        depth: 0.75,
+        dragControl: true,
+        initial: [0.2, -0.3],
+        textHeight: 20, txtOpt: true, wheelZoom: false, txtScale: 2
+    })) {
+        // TagCanvas failed to load
+        console.log("Sin inicializar");
+        $('#tagcloud').hide();
+    }
+    else {
+        console.log("Componente inicializado");
+    }
+});
---
+++ www/Scripts/Modules/Models/news-page.js
@@ -0,0 +1,45 @@
+﻿$(document).on('pageshow', '#news-page', function () {
+    console.log("inicializando noticias...");
+    var medias = ['2night', '101', 'ahora',
+        'cnn', 'CNNmexico', 'milenio',
+        'reforma', 'Televisa', 'tvazteca', 'zunoticia'];
+    var titles = ['Lorem ipsum dolor', 'Consectetur adipisicing elit',
+        'Sed do eiusmod', 'Tempor incididunt ut labore',
+        'Dolore magna aliqua', 'Ut enim ad minim ',
+        'Quis nostrud', 'Exercitation ullamco',
+        'Laboris nisi', 'Duis aute irure'];
+    var abstracts = ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur enim dui, semper sed accumsan vitae, eleifend varius orci. Ut sed justo quam.',
+    'In at risus ullamcorper, interdum erat a, iaculis tellus. Proin nec leo sed augue faucibus ullamcorper at quis magna. Nulla facilisi.',
+    'Mauris a nisl aliquam, laoreet augue volutpat, cursus tortor. Morbi quis justo tincidunt, molestie nisi ut, condimentum lacus.',
+    'Aenean eu vehicula dolor, vitae tincidunt lectus. Proin cursus nibh neque, at tincidunt dolor feugiat quis. ',
+    'Vestibulum elementum neque enim, ac aliquam leo fringilla ac. Pellentesque ut felis nec dui rutrum eleifend eget nec ante. ',
+    'Donec imperdiet ligula non orci tincidunt lacinia. Nullam pretium justo sit amet urna lacinia, non consequat metus tincidunt.']
+    var mediaTypes = ['internet',
+    'periodicos',
+    'radio',
+    'redes-sociales',
+    'revista',
+    'television'];
+    var newsTypes = ['analisis', 'articulo', 'carta',
+        'cartones', 'columna', 'comunicados',
+        'debate', 'desplegado', 'edicto-convocatoria',
+        'editorial', 'entrevista', 'esquela',
+        'estadistica', 'nota-comun', 'ocho-columnas',
+        'portada-seccion', 'primera-plana', 'publicidad',
+        'publireportaje', 'recuadro', 'redes-sociales', 'reportaje'];
+    for (var indice = 0; indice < 10; indice++) {
+        var media = medias[Math.floor(Math.random() * medias.length)];
+        var title = titles[Math.floor(Math.random() * titles.length)];
+        var abstract = abstracts[Math.floor(Math.random() * abstracts.length)];
+        var mediaType = mediaTypes[Math.floor(Math.random() * mediaTypes.length)];
+        var newsType = newsTypes[Math.floor(Math.random() * newsTypes.length)];
+        var newsItem = '<li data-icon="false">' + '<a href="news-detail.html" rel="external" class="news-itemlist">' +
+            '<img alt="' + media + '" src="Resources/Images/Media/' + media + '.png" class="media-logo" />' +
+            '<img alt="' + mediaType + '" src="Resources/Images/MediaTypes/' + mediaType + '.png" class="media-type" />' +
+            '<img alt="' + newsType + '" src="Resources/Images/NewsTypes/' + newsType + '.png" class="news-type" />' +
+            '<h2>' + title + '</h2><p>' +
+            abstract + '</p></a></li>';
+        $("#news-feed").append(newsItem);
+    }
+    $('#news-feed').listview('refresh');
+});
